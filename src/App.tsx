5409ad72a236b151986b45fefd02e1d862b71afb
Pochinsky <tomas.guttman@sansano.usm.cl>
2024-10-18 18:50:57 -0300

    ✨ listing the menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import MenuItem from "./components/MenuItem";
+import useOrder from "./hooks/useOrder";
+import { menuItems } from "./data/db";
+
 export default function App() {
+  const { addItem } = useOrder();
   return (
     <>
       <header className="bg-teal-400 py-5">
-        <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de propinas y consumo
+        </h1>
       </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-4 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} addItem={addItem} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import type { OrderItem, MenuItem } from "../types";
+
+export default function useOrder() {
+  const [order, setOrder] = useState<OrderItem[]>([]);
+
+  const addItem = (item: MenuItem) => {
+    const itemExists = order.find((orderItem) => orderItem.id === item.id);
+    if (itemExists) {
+      const updatedOrder = order.map((orderItem) =>
+        orderItem.id === item.id
+          ? { ...orderItem, quantity: orderItem.quantity + 1 }
+          : orderItem
+      );
+      setOrder(updatedOrder);
+    } else {
+      const newItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
+    }
+  };
+
+  return { addItem };
+}
