e6be1d3aa8987bb88ec2c66f5cec6c0c5736b27f
Pochinsky <tomas.guttman@sansano.usm.cl>
2024-10-18 18:09:00 -0300

    🎉 project initializated

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
+      </header>
+    </>
+  );
+}
